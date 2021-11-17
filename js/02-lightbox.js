9c264eaf4ba44bdb1c834216d817545e2e693746
sergey-lateralus <sergey.nezhyn@gmail.com>
2021-11-18 00:06:40 +0200

    Добавляет создание и рендер разметки для задания 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = createMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function createMarkup(array) {
+    return array.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>`
+    }).join('');
+};
